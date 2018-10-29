144b65cdae4e4c5574058428bf34aeec102d0521
matthewzpl <matthewzpl@163.com>
2018-10-29 12:01:16 +0800

    测试vim配置

--- lesson1/test1.rb
+++ lesson1/test1.rb
@@ -6,5 +6,16 @@
 puts 1..2
 puts 1...3
 puts "hello"*3
-print "hello".methods
+#print "hello".methods
 
+def myEach
+	["a","b","c"].each do
+		|x| print x,"--"
+	end
+end
+def myEach1
+	(1...6).each do
+		|i| p i
+	end
+end
+myEach1
